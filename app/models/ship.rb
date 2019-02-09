c4b7305bdc84442f9037723f0ed84b846302c5ea
Matthew McCormack <mmccormack1182@gmail.com>
2019-02-08 22:01:27 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,15 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  SHIPS = []
+
+  def initialize(name, type, booty)
+    self.name = name
+    self.type = type
+    self.booty = booty
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
 end
