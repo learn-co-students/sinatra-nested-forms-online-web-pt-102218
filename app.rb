232978c464e5e60d7c3626e9d0fa9c51432b0836
grant smith <grantsmith1256@gmail.com>
2019-02-09 21:35:40 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,13 +6,17 @@ module FormsLab
     # code other routes/actions here
 
     get '/' do
+      binding.pry
       erb :root
     end
 
     get '/new' do
+      erb :new
     end
 
+
     post '/pirates' do
+      erb :show
     end
 
 
