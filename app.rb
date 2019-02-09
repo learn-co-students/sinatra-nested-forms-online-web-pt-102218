02bda29aaccfd09b3d4f3ebcb617a9f819346461
grant smith <grantsmith1256@gmail.com>
2019-02-09 21:11:28 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,12 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+    "<h1>Welcome to the Nested Forms Lab!</h1>\n\n<p>In this lab, you'll
+    practice using nested forms in S... same name property. Capybara
+needs some sort of unique indentifier, so we use id. </li>\n\n</ul>\n" to include "let's navigate to the '/new'"
+
   end
 end
